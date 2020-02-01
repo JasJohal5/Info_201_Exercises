9ff7111e545faaf8aa5e416fc62b458915f76f08
JasJohal5 <59586328+JasJohal5@users.noreply.github.com>
2020-01-31 21:11:06 -0800

    Add files via upload

---
+++ exercise.R
@@ -0,0 +1,40 @@
+# Exercise 4: external data sets: Gates Foundation Educational Grants
+
+# Use the `read.csv()` functoin to read the data from the `data/gates_money.csv`
+# file into a variable called `grants` using the `read.csv()`
+# Be sure to set your working directory in RStudio, and do NOT treat strings as 
+# factors!
+grants <- read.csv ('data/gates_money.csv' stringsAsFactors = FALSE)
+
+# Use the View function to look at the loaded data
+View(grants)
+
+# Create a variable `organization` that contains the `organization` column of 
+# the dataset
+organization <- ncol(grants[grants$organization])
+
+# Confirm that the "organization" column is a vector using the `is.vector()` 
+# function. 
+# This is a useful debugging tip if you hit errors later!
+
+
+## Now you can ask some interesting questions about the dataset
+
+# What was the mean grant value?
+
+
+# What was the dollar amount of the largest grant?
+
+
+# What was the dollar amount of the smallest grant?
+
+
+# Which organization received the largest grant?
+grants[grants$total_amount == max(grants$total_amount), "organization"]
+grants$organization
+
+# Which organization received the smallest grant?
+
+
+# How many grants were awarded in 2010?
+
